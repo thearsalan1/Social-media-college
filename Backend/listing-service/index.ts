6d269d444a30e60f0869bbeecfbd3d818b68a5fd
thearsalan1 <arsalanloq@gmail.com>
2026-07-24 16:10:30 +0530

    feat: initialize listing-service with TypeScript, Redis configuration, and dependencies
    - Added package.json for listing-service with necessary dependencies and scripts.
    - Created redis.ts for Redis connection setup and error handling.
    - Added tsconfig.json for TypeScript configuration.

---
+++ Backend/listing-service/index.ts
@@ -0,0 +1,31 @@
+import helmet from "helmet";
+import "dotenv/config";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { connection } from "./src/config/redis.js";
+
+const app = express();
+app.use(helmet());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+  }),
+);
+app.use(express.json());
+
+app.use("/health", (req: Request, res: Response) => {
+  res
+    .status(200)
+    .json({ success: true, message: "listing-service is running " });
+});
+
+const token = await connection.get("RefreshToken");
+console.log(token);
+
+connection;
+
+const PORT = process.env.PORT || "5002";
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:5002`);
+});
