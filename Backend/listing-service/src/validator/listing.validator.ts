ca2c0fb77ba0d7b705b14ed11765ee0bca6dab81
thearsalan1 <arsalanloq@gmail.com>
2026-08-06 09:20:27 +0530

    feat: add Docker support and .dockerignore files for all services

--- Backend/listing-service/src/validator/listing.validator.ts
+++ Backend/listing-service/src/validator/listing.validator.ts
@@ -2,31 +2,31 @@ import z from "zod";
 import { ItemCategory, targetType } from "../types/types.js";
 
 export const createMarketplaceSchema = z.object({
-    title: z.string().min(3).max(100),
-    description: z.string().min(50).max(300),
-    price: z.number().min(10).max(10000),
-    category: z.enum(ItemCategory)
-})
+  title: z.string().min(3).max(100),
+  description: z.string().min(50).max(300),
+  price: z.coerce.number().min(10).max(10000),
+  category: z.enum(ItemCategory),
+});
 
 export const createSocialPostSchema = z.object({
-    content: z.string().min(5).max(500),
+  content: z.string().min(5).max(500),
 });
 
 export const createCommentSchema = z.object({
-    content: z.string().min(1).max(300),
-    targetType: z.enum(targetType),
-    targetId: z.string().min(1),
+  content: z.string().min(1).max(300),
+  targetType: z.enum(targetType),
+  targetId: z.string().min(1),
 });
 
 export const toggleLikeSchema = z.object({
-    targetType: z.enum(targetType),
-    targetId: z.string().min(1),
+  targetType: z.enum(targetType),
+  targetId: z.string().min(1),
 });
 
 export const reportSchema = z.object({
-    targetType: z.enum(targetType).or(z.literal("COMMENT")),
-    targetId: z.string().min(1),
-    reason: z.string().max(200).optional(),
+  targetType: z.enum(targetType).or(z.literal("COMMENT")),
+  targetId: z.string().min(1),
+  reason: z.string().max(200).optional(),
 });
 
 export type CreateMarketplaceInput = z.infer<typeof createMarketplaceSchema>;
